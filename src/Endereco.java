6362be81fda037187426a7212e42c57066c5473d
Daniel Penasio <dpensio@gmail.com>
2017-03-06 18:42:15 -0300

    Commit Inicial

---
+++ src/Endereco.java
@@ -0,0 +1,55 @@
+
+/**
+ *
+ * Daniel Penasio - dpenasio@gmail.com
+ * RA-266674
+ */
+public class Endereco {
+
+    private String endereco;
+    private String numero;
+    private String cep;
+    private String bairro;
+    private String cidade;
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,46 @@
+
+/**
+ *
+ * Daniel Penasio - dpenasio@gmail.com
+ * RA-266674
+ */
+public class Pessoa {
+
+    private String nome;
+    private String cpf;
+    private String dataNascimento;
+    private String relatorio;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getRelatorio() {
+        return relatorio;
+    }
+
+    public void setRelatorio(String relatorio) {
+        this.relatorio = relatorio;
+    }
+
+}
