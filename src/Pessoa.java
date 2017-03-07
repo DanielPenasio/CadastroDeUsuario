624c1a1d0c7d83bdc12756b31009fca4a3a2ac4c
Daniel Penasio <dpensio@gmail.com>
2017-03-06 22:34:48 -0300

    Commit projeto pronto

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -9,8 +9,44 @@ public class Pessoa {
     private String nome;
     private String cpf;
     private String dataNascimento;
+    private Endereco endereco;
+    private String sexo;
     private String relatorio;
+    private String interesses;
 
+    public String getInteresses() {
+        return interesses;
+    }
+
+    public void setInteresses(String interesses) {
+        this.interesses = interesses;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    private String estadoCivil;
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+    
+    public Endereco getEndereco() {
+        return endereco;
+    }
+    
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+    
     public String getNome() {
         return nome;
     }
